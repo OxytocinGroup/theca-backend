7642956783ea8c935797510a1f3528789249cabe
Leonid Petrushin <thunderleo@mail.ru>
2024-12-09 01:25:14 +0300

    add logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,9 +5,16 @@ import (
 
 	config "github.com/OxytocinGroup/theca-backend/internal/config"
 	di "github.com/OxytocinGroup/theca-backend/internal/di"
+	"github.com/OxytocinGroup/theca-backend/pkg/logger"
 )
 
 func main() {
+	err := logger.InitializeLogger("logs/app.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	defer logger.Sync()
+
 	config, err := config.LoadConfig()
 	if err != nil {
 		log.Fatal("cannot load config: ", err)
